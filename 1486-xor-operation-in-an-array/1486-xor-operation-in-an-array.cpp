df2033a393effb9799d68d0fc2fff0209683bed1
Dheeraj jami <dheerajjami2008@gmail.com>
2026-08-15 17:08:06 +0530

    Time: 0 ms (100%), Space: 7.9 MB (54.74%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        
+        int xor_res = 0,c =0;
+        for(int i = start ; c < n ; i += 2){
+            xor_res ^= i;
+            c++;
+        }
+        return xor_res;
     }
 };
